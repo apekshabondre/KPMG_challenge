393ab2b9626d33a43515777ee970a907d507a626
apekshabondre <107800645+apekshabondre@users.noreply.github.com>
2023-10-31 13:01:25 +0530

    Update get_value.py

--- Challenge3/get_value.py
+++ Challenge3/get_value.py
@@ -1,13 +1,21 @@
 def get_value(obj, key):
+    if not key:
+        return obj    
     keys = key.split('/')
+    val = obj
     for k in keys:
-        obj = obj[k]
-    return obj
+        if k in val:
+            val = val[k]
+        else:
+            return None
+    return val
 
-object = {"a":{"b":{"c":"d"}}}
-key = "a/b/c"
-print(get_value(object, key))
 
-object = {"x":{"y":{"z":"a"}}} 
-key = "x/y/z"
-print(get_value(object, key))
+if __name__ == "__main__":
+    object = {"a":{"b":{"c":"d"}}}
+    key = "a/b/c"
+    print("The Expected Value: ", get_value(object, key))
+
+    object = {"x":{"y":{"z":"a"}}} 
+    key = "x/y/z"
+    print("The Expected Value: ", get_value(object, key))
